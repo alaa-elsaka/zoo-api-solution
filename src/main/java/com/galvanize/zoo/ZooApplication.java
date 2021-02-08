e7c76e06f9a23aa540c08336fc0f37efefafbd50
Alaa Elsaka <alaa.elsaka@cognizant.com>
2021-02-08 15:19:17 -0600

    make it better

--- src/main/java/com/galvanize/zoo/ZooApplication.java
+++ src/main/java/com/galvanize/zoo/ZooApplication.java
@@ -2,8 +2,10 @@ package com.galvanize.zoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ZooApplication {
 
     public static void main(String[] args) {
